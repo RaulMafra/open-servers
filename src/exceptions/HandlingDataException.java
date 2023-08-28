72252cd947ef7bfb5c5405e2e33b307d68f33887
RaulMafra <raulcesar.sm@gmail.com>
2023-08-28 01:20:35 -0300

    feat: added class HandlingDataException

---
+++ src/exceptions/HandlingDataException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class HandlingDataException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public HandlingDataException(String msg) {
+		super(msg);
+	}
+
+}
