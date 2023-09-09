1a7f4af85d5cdd78dd64752968a5037eec1e16ba
RaulMafra <raulcesar.sm@gmail.com>
2023-09-08 21:44:50 -0300

    refactor: store method with unnecessary lines

--- src/model/service/HandlingData.java
+++ src/model/service/HandlingData.java
@@ -9,32 +9,31 @@ import model.entity.Store;
 import model.util.HandlingFile;
 
 public final class HandlingData {
-		
+
 	public static void store(String[] storesFull) {
 		List<Store> listStore = new ArrayList<>();
-		
+
 		for (String store : storesFull) {
 			storesFull = store.split(",");
-			if(!((storesFull[0].charAt(2) != 3) && (storesFull[0].charAt(2) != 3)) || 
-					!((storesFull[1].charAt(2) != 3) && (storesFull[0].charAt(2)) != 3)){
-				throw new HandlingDataException("Stores must be in the format \"xxx,yyy\", where 'x' is the store number and 'y' is the store name");
+			if (!(storesFull[0].charAt(2) != 3) || !(storesFull[1].charAt(2) != 3)) {
+				throw new HandlingDataException(
+						"Stores must be in the format \"xxx,yyy\", where 'x' is the store number and 'y' is the store name");
 			}
 			Short num = Short.parseShort(storesFull[0]);
 			String name = storesFull[1];
 			listStore.add(new Store(num, name));
 		}
-		
-		HandlingFile.writter(listStore);
+
+		HandlingFile.writterMiddleXML(listStore);
 	}
 
-	public static List<String> replace(Store stores, List<String> formatter) throws IndexOutOfBoundsException{
-			for (int i = 15; i < 18; i++) {
-				String line = formatter.get(i);
-				formatter.set(i, Pattern.compile("XXX").matcher(line).replaceAll(stores.getName()));
-			}
-		
+	public static List<String> replace(Store stores, List<String> formatter) throws IndexOutOfBoundsException {
+		for (int i = 0; i < 4; i++) {
+			String line = formatter.get(i);
+			formatter.set(i, Pattern.compile("XXX").matcher(line).replaceAll(stores.getName()));
+		}
+
 		return formatter;
 	}
-
 
 }
